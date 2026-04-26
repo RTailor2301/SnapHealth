937cf5ebb359874a5c4a1654acfa1fd332f605d7
RTailor2301 <roshan.tailor3456@gmail.com>
2026-04-26 16:52:25 -0400

    fixed deployment typo

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://snaphealth.vercel.app/" 
+        "https://snaphealth.vercel.app" 
     ],
     allow_credentials=True,
     allow_methods=["*"],
